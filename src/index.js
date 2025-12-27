b6a20a04f5982126d9623714421301c57eaec28b
23127264-23127316 <ttho23@clcl.fitus.edu.vn>
2025-12-27 18:32:50 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello DevOps - 23127264-23127316');
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
