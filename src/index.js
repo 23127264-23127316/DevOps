03d57c3cb606f9b29698e3568dc5ebaf2c286675
23127264-23127316 <ttho23@clcl.fitus.edu.vn>
2025-12-31 19:40:16 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps - 23127264-23127316');
+  res.send('Hello - 23127264-23127316');
 });
 
 app.listen(3000, () => {
